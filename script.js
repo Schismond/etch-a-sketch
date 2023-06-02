2b6e435de6661c89c5bf5db09f2fea021dfc80b3
Schismond <dadourachid720@gmail.com>
2023-06-02 18:40:46 +0100

    fix footer optimize js optimize html fix css add responsive layout

--- script.js
+++ script.js
@@ -1,142 +1,155 @@
-//colors
-let grayScale = document.getElementById('grey-shades');
-let colorBtn = document.getElementById('selectColor')
-let colorRange = document.getElementById('color-range');
-let RGB = document.getElementById('rainbow');
-let eraser = document.getElementById('eraser');
-let reset = document.getElementById('reset');
-let randomise = document.getElementById('randomise');
-//grid
-let gridSize = document.getElementById('grid-size');
-let gridSizeParagraph = document.getElementById('grid');
-let drawingArea = document.getElementById('playground');
-//functions
-function creatDivs(col) {
-    for(let i = 0; i < (col * col) ; i++) {
-        const div = document.createElement('div');
-        drawingArea.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
-        drawingArea.style.gridTemplateRows = `repeat(${col}, 1fr)`;
-        drawingArea.appendChild(div).classList.add('box');
-    }
+const grayScaleButton = document.getElementById("gray-scale");
+const colorButton = document.getElementById("select-color");
+const colorRangeInput = document.getElementById("color-range");
+const rgbButton = document.getElementById("rainbow");
+const eraserButton = document.getElementById("eraser");
+const resetButton = document.getElementById("reset-grid");
+const randomizeButton = document.getElementById("randomize");
+const gridSizeInput = document.getElementById("grid-size");
+const gridSizeParagraph = document.getElementById("grid-size-text");
+const drawingArea = document.getElementById("playground");
+
+// Grid
+function createGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const box = document.createElement("div");
+    drawingArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    drawingArea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    drawingArea.appendChild(box).classList.add("box");
+  }
 }
-creatDivs(16);
 
-function resetGrid(){
-    let boxes = document.querySelectorAll('.box');
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].remove();
-    }
-    drawingArea.style.gridTemplateColumns = '';
-    drawingArea.style.gridTemplateRows = '';
-   }
+function resetGrid() {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.remove();
+  });
+  drawingArea.style.gridTemplateColumns = "";
+  drawingArea.style.gridTemplateRows = "";
+}
 
-function setNewGridSize(){
-    var val = document.getElementById("grid-size").value;
-    gridSizeParagraph.innerText = (val + 'X' + val);
-    resetGrid();
-    creatDivs(val);
-    let boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = 'black';
-        });
+function setNewGridSize() {
+  const size = gridSizeInput.value;
+  gridSizeParagraph.innerText = `${size} X ${size}`;
+  resetGrid();
+  createGrid(size);
+  const boxes = document.querySelectorAll(".box");
+  const lastActiveButton = document.querySelector(".active");
+  lastActiveButton.classList.remove("active");
+  colorButton.classList.add("active");
+  let a = setNewColor();
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = a;
     });
-};
+  });
+}
 
-function setNewColor(){
-    let val = document.getElementById("color-range").value;
-    return val;
-};
-
-gridSize.addEventListener('change',setNewGridSize);
-
-colorRange.addEventListener('change',()=>{
-    let boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = colorRange.value;
-        });
-    });
-})
-
+// default behaviour
 window.onload = () => {
-    colorBtn.className += "active";
-    let boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = 'black' ;
-        });
+  colorButton.classList.add("active");
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = "black";
     });
+  });
+};
+
+// Color Functions
+function setColor(color) {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = color;
+    });
+  });
 }
 
-function gayColors(){
-    let boxes = document.querySelectorAll('.box');
-    let lastActiveBox = document.querySelector('.active');
-    lastActiveBox.classList.remove('active');
-    RGB.className += "active";
-    boxes.forEach(box => {box.addEventListener('mouseover', () => {
-        let RGBValue = [];
-        for(let i=0;i<3;i++){
-            RGBValue[i] = Math.floor(Math.random() * 257);
-        }
-        box.style.backgroundColor = 'rgb('+RGBValue.toString()+')';
-    });
-})};
-RGB.addEventListener('click',gayColors);
-eraser.addEventListener('click',erase);
-function erase(){
-    let boxes = document.querySelectorAll('.box');
-    let lastActiveBox = document.querySelector('.active');
-    lastActiveBox.classList.remove('active');
-    eraser.className += "active";
-    boxes.forEach(box => {box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = "white";
-        });
-    });
+function setNewColor() {
+  const color = colorRangeInput.value;
+  return color;
 }
 
-colorBtn.addEventListener('click',function(){
-    let boxes = document.querySelectorAll('.box');
-    let lastActiveBox = document.querySelector('.active');
-    lastActiveBox.classList.remove('active');
-    colorBtn.className += "active";
-    boxes.forEach(box => {box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = "black";
-        });
+// Event Listeners
+gridSizeInput.addEventListener("change", setNewGridSize);
+
+colorButton.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".box");
+    const lastActiveButton = document.querySelector(".active");
+    lastActiveButton.classList.remove("active");
+    colorButton.classList.add("active");
+    let a = setNewColor();
+    boxes.forEach((box) => {
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = a;
+      });
     });
 });
 
-reset.addEventListener('click',function(){
-    let boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {boxes
-        box.style.backgroundColor = "white";
-    });
+colorRangeInput.addEventListener("change", () => {
+  const color = setNewColor();
+  setColor(color);
 });
 
-randomise.addEventListener('click',function(){
-    let boxes = document.querySelectorAll('.box');
-    // let lastActiveBox = document.querySelector('.active');
-    // lastActiveBox.classList.remove('active');
-    // RGB.className += " active";
-    boxes.forEach(box => {boxes
-        let RGBValue = [];
-        for(let i=0;i<3;i++){
-            RGBValue[i] = Math.floor(Math.random() * 257);
-        }
-        box.style.backgroundColor = 'rgb('+RGBValue.toString()+')';    
+grayScaleButton.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".box");
+    const lastActiveButton = document.querySelector(".active");
+    lastActiveButton.classList.remove("active");
+    grayScaleButton.classList.add("active");
+    const opacityValues = new Array(boxes.length).fill(0);
+    boxes.forEach((box, index) => {
+      box.addEventListener("mouseover", () => {
+        opacityValues[index] += 0.1;
+        box.style.backgroundColor = `rgba(0, 0, 0, ${opacityValues[index]})`;
+      });
     });
+  });
+
+rgbButton.addEventListener("click", () => {
+  const boxes = document.querySelectorAll(".box");
+  const lastActiveButton = document.querySelector(".active");
+  lastActiveButton.classList.remove("active");
+  rgbButton.classList.add("active");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+      // Generate three different colors directly and assign them as CSS values
+      const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)})`;
+      box.style.backgroundColor = randomColor;
+    });
+  });
 });
 
-grayScale.addEventListener('click',function(){
-    let boxes = document.querySelectorAll('.box');
-    let lastActiveBox = document.querySelector('.active');
-    lastActiveBox.classList.remove('active');
-    grayScale.className += "active";
-    let T=[];
-    for(let j=0;j<boxes.length;j++){
-        T[j]=0;
-    }
-    for(let i=0;i<boxes.length;i++){
-        boxes[i].addEventListener('mouseover', () => {
-            T[i]=T[i]+0.1;
-            boxes[i].style.backgroundColor = 'rgba(0, 0, 0, '+ T[i] +')';
-    }
-    );}
+eraserButton.addEventListener("click", () => {
+  const boxes = document.querySelectorAll(".box");
+  const lastActiveButton = document.querySelector(".active");
+  lastActiveButton.classList.remove("active");
+  eraserButton.classList.add("active");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = "white";
+    });
+  });
 });
+
+resetButton.addEventListener("click", () => {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.style.backgroundColor = "white";
+  });
+});
+
+randomizeButton.addEventListener("click", () => {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`;
+    box.style.backgroundColor = randomColor;
+  });
+});
+
+// Initial Grid Creation
+createGrid(16);
